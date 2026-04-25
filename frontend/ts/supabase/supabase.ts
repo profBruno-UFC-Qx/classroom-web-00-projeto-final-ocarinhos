1035a9be53cdc01b5f68c9b4590ac76f52438034
narciso-roberto <narcisoroberto1369@gmail.com>
2026-04-25 11:00:02 -0300

    conflitos

--- frontend/ts/supabase/supabase.ts
+++ frontend/ts/supabase/supabase.ts
@@ -1,14 +1,12 @@
 // @ts-ignore
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
-import { SupabaseClient } from "@supabase/supabase-js";
-
 import ENV from "../config.js";
 
 // const VITE_SUPABASE_URL = "https://rtowwxvptqgljsiitalj.supabase.co";
 // const VITE_SUPABASE_PUBLISHABLE_KEY =
 //   "sb_publishable_k-sbqrJ3Rqhhp28fb1XUew_GsBcpl_f";
 
-const supabase: SupabaseClient = createClient(
+const supabase = createClient(
   ENV.VITE_SUPABASE_URL,
   ENV.VITE_SUPABASE_PUBLISHABLE_KEY
 );
