1e4a16be821640feaaa564a488d210cdc78fba1c
narciso-roberto <narcisoroberto1369@gmail.com>
2026-04-26 17:15:35 -0300

    validando

--- frontend/ts/register/register.ts
+++ frontend/ts/register/register.ts
@@ -1,42 +1,96 @@
-import { AuthError } from "@supabase/supabase-js";
 import { supabase } from "../supabase/supabase.js";
+
+interface LoginData {
+  name: string;
+  email: string;
+  IES: string;
+  curso: string;
+  senha: string;
+  confrmarsenha: string;
+}
 
 const form = document.querySelector(".form");
 
 if (form instanceof HTMLFormElement) {
-  const nome = document.getElementById("nome");
-  const email = document.getElementById("email");
-  const IES = document.getElementById("ies");
-  const curso = document.getElementById("curso");
-  const senha = document.getElementById("senha");
-  const confrmarsenha = document.getElementById("confirmar-senha");
+  const nome = document.getElementById("nome") as HTMLInputElement;
+  const email = document.getElementById("email") as HTMLInputElement;
+  const IES = document.getElementById("ies") as HTMLSelectElement;
+  const curso = document.getElementById("curso") as HTMLInputElement;
+  const senha = document.getElementById("senha") as HTMLInputElement;
+  const confrmarsenha = document.getElementById(
+    "confirmar-senha"
+  ) as HTMLInputElement;
+
+  const elements = [nome, email, IES, curso, senha, confrmarsenha];
+
+  function showError(selector: string, msg: string) {
+    const erroBox = document.querySelector(`.input-group .${selector}`);
+    if (erroBox) {
+      erroBox.innerHTML = msg;
+    }
+  }
+
+  function checkLogin(obj: unknown): obj is LoginData {
+    if (
+      !(
+        obj &&
+        typeof obj == "object" &&
+        "confirmar-senha" in obj &&
+        "curso" in obj &&
+        "email" in obj &&
+        "ies" in obj &&
+        "nome" in obj &&
+        "senha" in obj
+      )
+    ) {
+      return false;
+    }
+
+    const existeElementosVazios = elements.filter((element) => {
+      if (!element.value) {
+        showError(element.name, "Digite um valor");
+        return true;
+      } else {
+        showError(element.name, "");
+      }
+    });
+
+    if (!existeElementosVazios) {
+      return false;
+    }
+
+    if (obj.senha && obj.senha != obj["confirmar-senha"]) {
+      showError("senha", "senhas diferentes");
+      return false;
+    }
+
+    if (typeof obj.senha == "string" && obj.senha.length < 6) {
+      showError("senha", "Digite ate 6 caracteres");
+      return false;
+    }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (typeof obj.email == "string" && !emailRegex.test(obj.email)) {
+      showError("email", "Email inválido");
+      return false;
+    }
+
+    return true;
+  }
 
   const onSubmit = async (event: SubmitEvent) => {
     event.preventDefault();
 
-    if (
-      nome instanceof HTMLInputElement &&
-      email instanceof HTMLInputElement &&
-      IES instanceof HTMLSelectElement &&
-      curso instanceof HTMLInputElement &&
-      senha instanceof HTMLInputElement &&
-      confrmarsenha instanceof HTMLInputElement
-    ) {
-      const objLogin = {
-        name: nome.value,
-        email: email.value,
-        IES: IES.value,
-        curso: curso.value,
-        senha: senha.value,
-        confrmarsenha: confrmarsenha.value,
-      };
+    const formData = new FormData(form);
+    const objLogin = Object.fromEntries(formData.entries());
+    console.log(objLogin);
 
+    if (checkLogin(objLogin)) {
       const { data, error } = await supabase.auth.signUp({
         email: objLogin.email,
         password: objLogin.senha,
       });
-
-      console.log(error?.cause)
     }
   };
 
