0b37f594135dbded5fb2092ccbe35bdf219fae68
narciso-roberto <narcisoroberto1369@gmail.com>
2026-05-14 09:40:08 -0300

    feat: tabela de motoristas ADM

---
+++ frontend/ts/admMotoristas/admMotoristas.ts
@@ -0,0 +1,58 @@
+import { supabase } from "../supabase/supabase.js";
+import showTopMessage from "../utils/showMsg.js";
+
+interface motoristasInterface {
+  nome: string;
+  kmAtual: number;
+  onibus: {
+    nome: string;
+  };
+}
+
+const { data, error } = (await supabase
+  .from("motoristas")
+  .select("nome, onibus (nome), kmAtual")) as {
+  data: motoristasInterface[] | null;
+  error: any;
+};
+
+if (error) {
+  showTopMessage("Não foi possível fazer o fetch dos motoristas.", "error");
+}
+
+const listaMotoristas = data;
+
+if (listaMotoristas) {
+  const motoristasTable = document.querySelector(
+    ".motoristasTable tbody"
+  ) as HTMLTableSectionElement;
+
+  listaMotoristas.forEach((motorista) => {
+    const tr = document.createElement("tr");
+
+    tr.innerHTML = `
+  <td class='name'>
+    <i class='bi bi-mortarboard' aria-hidden='true'></i>
+    ${motorista.nome}
+  </td>
+
+  <td>${motorista.onibus.nome}</td>
+
+  <td>
+    <span class='qtdAlunos'>${motorista.kmAtual} KM</span>
+  </td>
+
+  <td class='buttons'>
+    <button type='button' aria-label='Editar' class='edit'>
+      <i class='bi bi-pencil'></i>
+    </button>
+
+    <button type='button' aria-label='Excluir' class='delete'>
+      <i class='bi bi-trash'></i>
+    </button>
+  </td>
+`;
+
+    motoristasTable.appendChild(tr);
+  });
+}
