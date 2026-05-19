fd77caf60d47016f5170c97eb0b09a4cc51c2d89
narciso-roberto <narcisoroberto1369@gmail.com>
2026-05-19 17:49:35 -0300

    resolvendo conflitos

--- frontend/ts/register/register.ts
+++ frontend/ts/register/register.ts
@@ -66,15 +66,6 @@ preencherFaculdades();
 const form = document.querySelector(".form");
 
 if (form instanceof HTMLFormElement) {
-  const nome = document.getElementById("nome") as HTMLInputElement;
-  const email = document.getElementById("email") as HTMLInputElement;
-  const IES = document.getElementById("ies") as HTMLSelectElement;
-  const curso = document.getElementById("curso") as HTMLInputElement;
-  const senha = document.getElementById("senha") as HTMLInputElement;
-  const confrmarsenha = document.getElementById(
-    "confirmarsenha"
-  ) as HTMLInputElement;
-
   const onSubmit = async (event: SubmitEvent) => {
     event.preventDefault();
 
@@ -108,7 +99,7 @@ if (form instanceof HTMLFormElement) {
       });
 
       if (!error) {
-        window.location.href = "/index.html";
+        window.location.href = "dashBoardAluno.html";
       }
 
       if (error) {
