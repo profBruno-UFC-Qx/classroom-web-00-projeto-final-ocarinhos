9f4894a84fc7459a9421a8f2f7df60d772aa8cdf
narciso-roberto <narcisoroberto1369@gmail.com>
2026-06-01 08:41:37 -0300

    tirando headers e integrando aside

--- frontend/ts/admMotoristas/admMotoristas.ts
+++ frontend/ts/admMotoristas/admMotoristas.ts
@@ -1,5 +1,7 @@
 import { supabase } from "../supabase/supabase.js";
 import showTopMessage from "../utils/showMsg.js";
+import { renderizarSidebar } from "../components/sidebar.js";
+renderizarSidebar("sidebar-container", "dashboard");
 
 interface motoristasInterface {
   nome: string;
