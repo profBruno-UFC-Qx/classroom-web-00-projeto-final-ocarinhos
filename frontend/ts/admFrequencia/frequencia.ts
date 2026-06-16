cce37d21ff885fbf4ef26c89dd632781e7b7fbbf
mateus124 <alvesmateusq00@gmail.com>
2026-06-16 17:32:52 -0300

    feat: consulta diária de atribuições

--- frontend/ts/admFrequencia/frequencia.ts
+++ frontend/ts/admFrequencia/frequencia.ts
@@ -1,2 +1,41 @@
 import { renderizarSidebar } from "../components/sidebarADM.js";
 renderizarSidebar("sidebar-container", "frequencia");
+
+const dataInput = document.querySelector<HTMLInputElement>("#dataEscala");
+const textoData = document.querySelector<HTMLElement>("#dataAtual");
+const botaoAnterior = document.querySelector<HTMLButtonElement>(".prev-day");
+const botaoProximo = document.querySelector<HTMLButtonElement>(".next-day");
+let dataSelecionada = new Date();
+
+if (dataInput) {
+  dataInput.value = new Date().toISOString().slice(0, 10);
+}
+
+function atualizarDataTela() {
+  if (textoData) {
+    textoData.textContent =
+      dataSelecionada.toLocaleDateString("pt-BR",
+        {
+          day: "2-digit",
+          month: "long",
+          year: "numeric",
+        }
+      );
+  }
+}
+
+botaoAnterior?.addEventListener("click", () => {
+  dataSelecionada.setDate(dataSelecionada.getDate() - 1);
+
+  atualizarDataTela();
+  // renderizarTabela();
+});
+
+botaoProximo?.addEventListener("click", () => {
+  dataSelecionada.setDate(dataSelecionada.getDate() + 1);
+
+  atualizarDataTela();
+  // renderizarTabela();
+});
+
+atualizarDataTela();
