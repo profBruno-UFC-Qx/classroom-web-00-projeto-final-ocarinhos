6b01115c5e6336e08873a1f24d486242f58b35b8
mateus124 <alvesmateusq00@gmail.com>
2026-06-18 19:36:53 -0300

    feat: consultar rotas integrada

--- frontend/ts/consultar-rota/consultar-rota.ts
+++ frontend/ts/consultar-rota/consultar-rota.ts
@@ -1,3 +1,239 @@
+import { supabase } from "../supabase/supabase.js";
 import { renderizarSidebar } from "../components/sidebar.js";
-
 renderizarSidebar("sidebar-container", "rota");
+const tbody = document.querySelector(".transporte-table tbody");
+const filtro = document.getElementById("filtroOnibus") as HTMLSelectElement;
+const tbodyPassageiros = document.querySelector(".passengers-table tbody");
+const badge = document.querySelector(".weekday-badge");
+
+let diaAtual: DiaSemana = "segunda";
+
+function preencherFiltroOnibus(dados: any[]) {
+  filtro.innerHTML = '<option value="">Todos</option>';
+
+  dados.forEach(item => {
+    filtro.innerHTML += `
+      <option value="${item.id}">
+        ${item.motoristas.onibus.nome}
+      </option>
+    `;
+  });
+}
+
+function proximaData(dia: "segunda" | "terca" | "quarta" | "quinta" | "sexta") {
+  const mapa: Record<DiaSemana, number> = {
+    segunda: 1,
+    terca: 2,
+    quarta: 3,
+    quinta: 4,
+    sexta: 5,
+  };
+
+  const hoje = new Date();
+  let delta = (mapa[dia] - hoje.getDay() + 7) % 7;
+
+  if (delta === 0) {
+    delta = 7;
+  }
+
+  const data = new Date();
+  data.setDate(data.getDate() + delta);
+
+  return data.toISOString().split("T")[0];
+}
+
+function renderizarTabelaTransportes(dados: any[]) {
+  if (!tbody) {
+    return;
+  }
+
+  tbody.innerHTML = "";
+
+  dados.forEach(item => {
+    tbody.innerHTML += `
+      <tr>
+        <td>
+          <strong>
+            ${item.motoristas.onibus.nome}
+          </strong>
+        </td>
+
+        <td>
+          ${item.faculdades.nome}
+        </td>
+
+        <td>
+          ${item.rotasComplementares.nome}
+        </td>
+
+        <td>
+          ${item.motoristas.nome}
+        </td>
+      </tr>
+    `;
+  });
+}
+
+type DiaSemana = | "segunda" | "terca" | "quarta" | "quinta" | "sexta";
+
+async function carregarTransportes(dia: DiaSemana) {
+  const dataSelecionada = proximaData(dia);
+
+  const { data: registroData } = await supabase
+    .from("Datas")
+    .select("id")
+    .eq("data_ocorrencia", dataSelecionada);
+
+  if (!registroData?.length) {
+    return;
+  }
+
+  const ids = registroData.map((d: { id: number }) => d.id);
+
+  const { data } = await supabase
+    .from("motoristaAssociacao")
+    .select(`
+      *,
+      motoristas(
+        nome,
+        onibus(
+          id,
+          nome
+        )
+      ),
+      faculdades(
+        nome
+      ),
+      rotasComplementares(
+        nome
+      )
+    `)
+    .in("Data_id", ids);
+
+  renderizarTabelaTransportes(data ?? []);
+  preencherFiltroOnibus(data ?? []);
+  if (data?.length) {
+    await carregarPassageiros(data[0].Faculdade_id, data[0].RotaComplementar_id, dia);
+  }
+}
+
+function renderizarPassageiros(passageiros: any[]) {
+
+  if (!tbodyPassageiros) {
+    return;
+  }
+
+  tbodyPassageiros.innerHTML = "";
+
+  passageiros.forEach(p => {
+    tbodyPassageiros.innerHTML += `
+      <tr>
+        <td>
+          ${p.usuarios.nome}
+        </td>
+
+        <td>
+          <span class="tag">
+            ${p.usuarios.curso}
+          </span>
+        </td>
+      </tr>
+    `;
+  });
+}
+
+async function carregarPassageiros(faculdadeId: number, rotaId: number, dia: DiaSemana) {
+  const dataSelecionada = proximaData(dia);
+
+  const { data } = await supabase
+    .from("Datas")
+    .select(`
+      *,
+      ParticipaFreq(
+        *,
+        usuarios(
+          nome,
+          curso
+        )
+      )
+    `)
+    .eq(
+      "data_ocorrencia",
+      dataSelecionada
+    );
+
+  if (!data) {
+    return;
+  }
+
+  const passageiros = data
+    .map((d: any) => d.ParticipaFreq)
+    .filter(
+      (p: any) =>
+        p.ida_destino === faculdadeId &&
+        p.rotaComplementar === rotaId
+    );
+
+  renderizarPassageiros(passageiros);
+}
+
+filtro.addEventListener("change", async () => {
+    const id = Number(filtro.value);
+
+    if (!id) {
+        return;
+    }
+
+    const { data } = await supabase
+        .from(
+          "motoristaAssociacao"
+        )
+        .select("*")
+        .eq("id", id)
+        .single();
+
+    if (!data) {
+        return;
+    }
+
+    carregarPassageiros(data.Faculdade_id, data.RotaComplementar_id, diaAtual);
+  }
+);
+
+function atualizarBadge(dia: DiaSemana) {
+  if (!badge) {
+    return;
+  }
+
+  const nomes = {
+    segunda: "Segunda-feira",
+    terca: "Terça-feira",
+    quarta: "Quarta-feira",
+    quinta: "Quinta-feira",
+    sexta: "Sexta-feira"
+  };
+
+  badge.textContent = nomes[dia];
+}
+
+document.querySelectorAll(".tab").forEach(btn => {
+  btn.addEventListener("click", async () => {
+
+    document.querySelectorAll(".tab").forEach(tab => {
+      tab.classList.remove("active");
+    });
+    btn.classList.add("active");
+
+    const dia = btn.getAttribute("data-dia");
+    if (!dia) {
+      return;
+    }
+
+    diaAtual = dia as DiaSemana;
+    atualizarBadge(diaAtual);
+
+    await carregarTransportes(diaAtual);
+  });
+});
+
+await carregarTransportes("segunda");
